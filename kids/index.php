cf5376e39467c703ba89fedfee8139874ce8145b
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-15 15:34:18 -0500

    changed the description

--- kids/index.php
+++ kids/index.php
@@ -4,8 +4,8 @@
 ?>
 	     <div class="jumbotron" style="padding-top:100px;">
       <div class="container">
-        <h1>Hello, world!</h1>
-        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+        <h1>Kids Area!</h1>
+        <p>Thanks for visiting the kids page on the St. Augustine School Website</p>
         <p><a class="btn btn-primary btn-lg" role="button">Learn more &raquo;</a></p>
       </div>
     </div>
