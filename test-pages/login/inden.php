bb8637cac744ce550fad40721cc474dbf69f101c
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-15 09:54:46 -0500

    Made the sql querquery correctly

--- test-pages/login/inden.php
+++ test-pages/login/inden.php
@@ -6,7 +6,7 @@ require_once 'core/init.php';
 //die(Config::get('mysql/password'));
 
 
-$user = DB::getInstance()->query("SELECT username FROM users WHERE username='asd'");
+$user = DB::getInstance()->query("SELECT username FROM users WHERE username='matt'");
 
 //var_dump($user->error());
 ?><br><?php
