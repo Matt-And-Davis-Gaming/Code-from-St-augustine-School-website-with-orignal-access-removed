f37e1fb7dfea75104abf23a0d26b7a8a40a39dde
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-15 18:29:09 -0500

    changed back

--- test-pages/login/inden.php
+++ test-pages/login/inden.php
@@ -6,7 +6,7 @@ require_once 'core/init.php';
 //die(Config::get('mysql/password'));
 
 
-$user = DB::getInstance()->get('users', array('username', '=', 'matdt'));
+$user = DB::getInstance()->get('users', array('username', '=', 'matt'));
 
 //var_dump($user->error());
 ?><br><?php
