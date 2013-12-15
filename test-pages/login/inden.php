d78849b94fefcc8e21e5ef5e2ed655d14a1ef3a7
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-15 18:48:45 -0500

    cleared index

--- test-pages/login/inden.php
+++ test-pages/login/inden.php
@@ -3,17 +3,5 @@
 ini_set('display_errors', '1');
 error_reporting(-1);
 require_once 'core/init.php';
-//die(Config::get('mysql/password'));
 
-
-$user = DB::getInstance()->get('users', array('username', '=', 'matt'));
-
-//var_dump($user->error());
-?><br><?php
-if(!$user->count()){
-	echo 'No User Found';
-}else{
-	echo $user->first()->username;
-}
-//echo $lev = E_ALL & !E_NOTICE & !E_DEPRECATED;
-//http://www.youtube.com/watch?feature=player_detailpage&v=PaBWDOBFxDc#t=567
+$user = DB::getInstance();
