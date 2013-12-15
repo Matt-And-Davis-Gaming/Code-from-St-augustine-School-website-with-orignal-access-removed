dc564e6073604cae84a706e509177712166fa2b3
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-15 18:52:48 -0500

    cleaned abit and changed inden

--- test-pages/login/inden.php
+++ test-pages/login/inden.php
@@ -4,4 +4,8 @@ ini_set('display_errors', '1');
 error_reporting(-1);
 require_once 'core/init.php';
 
-$user = DB::getInstance();
+$user = DB::getInstance()->insert('users', array(
+	'username'	=> 'Dale',
+	'password'	=> 'hashed_passphrase',
+	'salt'		=> 'salt'
+));
