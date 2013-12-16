c6d3ae3c7b019b14ef36f4a85e3399fd73754373
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-15 19:32:56 -0500

    changed inden

--- test-pages/login/inden.php
+++ test-pages/login/inden.php
@@ -4,8 +4,8 @@ ini_set('display_errors', '1');
 error_reporting(-1);
 require_once 'core/init.php';
 
-$user = DB::getInstance()->insert('users', array(
+$user = DB::getInstance()->update('users', 3, array(
 	'username'	=> 'Dale',
-	'password'	=> 'hashed_passphrase',
+	'password'	=> 'new_hashed_passphrase',
 	'salt'		=> 'salt'
 ));
