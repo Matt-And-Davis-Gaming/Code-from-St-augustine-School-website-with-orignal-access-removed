1bf78bcf75f41685049fe2fffb8777920709adf5
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-17 18:21:42 -0500

    encased it all in a table

--- test-pages/login/register.php
+++ test-pages/login/register.php
@@ -1,21 +1,27 @@
 <form aaction="" method="post">
-	<div class="field">
-		<label for="username">Please choose a username</label>
-		<input type="text" name="username" id="username" value="" autocomplete="off" />
-	</div>
-	<div class="field">
-		<label for="password">Please choose a password</label>
-		<input type="password" name="password" id="password" />
-	</div>
-	<div class="field">
-		<label for="password_again">Repeat your password please</label>
-		<input type="password" name="password_again" id="password_again" />
-	</div>
-	<div class="field">
-		<label for="name">Please enter your name</label>
-		<input type="text" name="name" id="name" value="" autocomplete="off" />
-	</div>
+	<table>
+		<thead>
 
-	<input type="submit" value="Register" />
+		</thead>
+		<tbody>
+			<tr><div class="field">
+				<td><label for="username">Please choose a username</label></td>
+				<td><input type="text" name="username" id="username" value="" autocomplete="off" /></td>
+			</div></tr>
+			<tr><div class="field">
+				<td><label for="password">Please choose a password</label></td>
+				<td><input type="password" name="password" id="password" /></td>
+			</div></tr>
+			<tr><div class="field">
+				<td><label for="password_again">Repeat your password please</label></td>
+				<td><input type="password" name="password_again" id="password_again" /></td>
+			</div></tr>
+			<tr><div class="field">
+				<td><label for="name">Please enter your name</label></td>
+				<td><input type="text" name="name" id="name" value="" autocomplete="off" /></td>
+			</div></tr>
 
+			<tr><td colspan="2" style="text-align:center;float:center;"><input type="submit" value="Register" /></td></tr>
+		</tbody>
+	</table>
 </form>
