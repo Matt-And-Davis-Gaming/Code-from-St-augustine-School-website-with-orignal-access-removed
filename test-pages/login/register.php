95700acdd1ac17ead9aae32746801400d5354fcb
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-17 18:17:51 -0500

    Built Register.php

---
+++ test-pages/login/register.php
@@ -0,0 +1,21 @@
+<form aaction="" method="post">
+	<div class="field">
+		<label for="username">Please choose a username</label>
+		<input type="text" name="username" id="username" value="" autocomplete="off" />
+	</div>
+	<div class="field">
+		<label for="password">Please choose a password</label>
+		<input type="password" name="password" id="password" />
+	</div>
+	<div class="field">
+		<label for="password_again">Repeat your password please/label>
+		<input type="password" name="password_again" id="password_again" />
+	</div>
+	<div class="field">
+		<label for="name">Please enter your name</label>
+		<input type="text" name="name" id="name" value="" autocomplete="off" />
+	</div>
+
+	<input type="submit" value="Register" />
+
+</form>
