89083ee537fe4ca3f6534d7bd53d5d2ad4e58379
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-17 21:15:20 -0500

    added basic Validate

---
+++ test-pages/login/classes/Validate.php
@@ -0,0 +1,20 @@
+<?php
+class Validate{
+	private $_passed 	= false,
+			$_errors 	= array(),
+			$_db		= null;
+
+	public function __construct()
+	{
+		$this->_db = DB::getInstance();
+	}
+
+	public function check($source, $items = array())
+	{
+		foreach ($items as $item => $rules) {
+			foreach ($rules as $rule => $rule_value) {
+				echo "{$item} {$rule} must be {$rule_value}";
+			}
+		}
+	}
+}
