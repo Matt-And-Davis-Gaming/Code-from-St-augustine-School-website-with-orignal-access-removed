fa9df5de4851594d6285498b0c4a454f191e222e
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-21 09:28:53 -0500

    moved if back

--- test-pages/login/classes/Validate.php
+++ test-pages/login/classes/Validate.php
@@ -19,6 +19,9 @@ class Validate{
 				# echo "{$item} {$rule} must be {$rule_value}!<br>";
 
 				$value = trim($source[$item]);
+				if ($rule === 'required' && empty($value)) {
+					$this->addError($items[$item]['name'] . " is required");
+				}
 				if (!empty($value)) {
 					switch ($rule) {
 						case 'min':
@@ -37,11 +40,6 @@ class Validate{
 						
 						case 'unique':
 							# code...
-							break;
-						case 'required':
-							if ($rule === 'required' && empty($value)) {
-								$this->addError($items[$item]['name'] . " is required");
-							}
 							break;
 						
 						default:
