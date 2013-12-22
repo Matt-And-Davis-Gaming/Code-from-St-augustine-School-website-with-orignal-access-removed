2cb6e454c9257f73599a004ab7ab4a0cd2f8ee3a
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-21 21:02:56 -0500

    commented print_r

--- test-pages/login/flash.php
+++ test-pages/login/flash.php
@@ -1,6 +1,7 @@
 <?php
 require '/var/www/test-pages/login/core/init.php';
 if(Session::exists('success')){
-  echo "<pre>", print_r($_SESSION), "</pre>";
+  # echo "<pre>", print_r($_SESSION), "</pre>";
   echo Session::flash("success");
+  # header("");
 }
