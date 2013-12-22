a066604cd3a47550cac7e92a9881cd55ffe62d6c
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-22 12:44:52 -0500

    finished hash.php

---
+++ test-pages/login/classes/User.php
@@ -0,0 +1,17 @@
+<?php
+class Users{
+	private $_db;
+
+	public function __construct($user = null)
+	{
+		$this->_db = DB::getInstance();
+	}
+
+	public function create($fields = array())
+	{
+		if ($this->_db->insert('users', $fields)) {
+			throw new Exception("There was an error. Please read our <a href=\"/help/policies#\"></a>);
+			
+		}
+	}
+}
