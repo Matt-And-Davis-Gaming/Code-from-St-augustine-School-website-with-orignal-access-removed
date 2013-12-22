66e6fb88140aa02bab30d1f0765429493d4d9b9a
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-22 12:13:04 -0500

    added header redirect

--- test-pages/login/flash.php
+++ test-pages/login/flash.php
@@ -3,5 +3,5 @@ require '/var/www/test-pages/login/core/init.php';
 if(Session::exists('success')){
   # echo "<pre>", print_r($_SESSION), "</pre>";
   echo Session::flash("success");
-  # header("");
+  header("Refresh: 3; inden.php")
 }
