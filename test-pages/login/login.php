b09091a6fcdfa0c2eab14aee3df11555aec8c4d0
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-22 15:49:34 -0500

    saved progress

--- test-pages/login/login.php
+++ test-pages/login/login.php
@@ -1,5 +1,6 @@
 <form method="post" action="" >
 	<div>
-		<label for="username"></label>
+		<label for="username">Username </label>
+		<input type="text" name="username" id="username" />
 	</div>
 </form>
