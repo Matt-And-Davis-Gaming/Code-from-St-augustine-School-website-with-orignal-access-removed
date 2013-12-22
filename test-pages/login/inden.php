40f44d7f503d2de30a67020462c98fd7bca8a47c
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-22 15:17:07 -0500

    removed update syntax

--- test-pages/login/inden.php
+++ test-pages/login/inden.php
@@ -4,9 +4,9 @@ ini_set('display_errors', '1');
 error_reporting(-1);
 require_once 'core/init.php';
 
-$user = DB::getInstance()->update('users', 3, array(
+/*$user = DB::getInstance()->update('users', 3, array(
 	'username'	=> 'Dale',
 	'password'	=> 'new_hashed_passphrase',
 	'salt'		=> 'salt',
 	'name'		=> 'Dale Garrett'
-));
+));*/
