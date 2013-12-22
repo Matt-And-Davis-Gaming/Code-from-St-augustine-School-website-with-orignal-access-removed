2128d54720b4b6aae5c33625d7745866e9a289c5
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-21 19:57:02 -0500

    compleatesession

--- test-pages/login/classes/Session.php
+++ test-pages/login/classes/Session.php
@@ -1,7 +1,24 @@
 <?php
 class Session{
+
+	public static function exists($name)
+	{
+		return (isset($_SESSION[$name])) ? true : flase;
+	}
 	public static function put($name, $value)
 	{
 		return $_SESSION[$name] = $value;
 	}
+
+	public static function get($name)
+	{
+		return $_SESSION[$name];
+	}
+	public static function delete($name)
+	{
+		if (self::exists($name)) {
+			unset($_SESSION[$name]);
+		}
+	}
+
 }
