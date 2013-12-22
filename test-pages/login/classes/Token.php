9a587cba8d01f20f97cd14495f747ae58e179e69
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-21 19:53:37 -0500

    madae code for check token

--- test-pages/login/classes/Token.php
+++ test-pages/login/classes/Token.php
@@ -4,4 +4,16 @@ class Token{
 	{
 		return Session::put(Config::get("session/CSRF_protect"), sha1(uniqid()));
 	}
+	public static function check($token)
+	{
+		$token_name = Config::get("session/CSRF_protect");
+
+		if (Session::exists($token_name) && $token === Session::get($token_name)) {
+			Session::delete($token_name);
+			return true;
+		}
+
+		return false;
+
+	}
 }
