a55d164a6b0b2138445659caaacf5380653c225b
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-22 19:25:09 -0500

    updated type

--- test-pages/login/login.php
+++ test-pages/login/login.php
@@ -17,7 +17,7 @@
 			</div>
 			<div class="filed">
 				<tr><td><label for="password">Password </label></td>
-				<td><input type="text" name="password" id="password" autocompleate="off" /></td></tr>
+				<td><input type="password" name="password" id="password" autocompleate="off" /></td></tr>
 			</div>
 
 
