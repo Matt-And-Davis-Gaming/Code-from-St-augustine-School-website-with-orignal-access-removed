c677da059d8cfd09c72da1d6131d7cdbec291377
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-22 19:19:23 -0500

    added basic start

--- test-pages/login/login.php
+++ test-pages/login/login.php
@@ -1,6 +1,14 @@
 <form method="post" action="" >
-	<div>
+	<div class="field">
 		<label for="username">Username </label>
-		<input type="text" name="username" id="username" />
+		<input type="text" name="username" id="username" autocompleate="off" />
 	</div>
+	<div class="filed">
+		<label for="password">Password </label>
+		<input type="text" name="password" id="password" autocompleate="off" />
+	</div>
+
+
+	<input type="hidden" name="token" value="<?php echo Token::generate(); ?>" />
+	<input type="submit" value="Log In" />
 </form>
