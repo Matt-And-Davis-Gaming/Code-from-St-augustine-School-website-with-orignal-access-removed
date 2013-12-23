430e1b47cd655a75b51bfbfb777b23fbddccff00
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-22 19:31:34 -0500

    added ckecked stuff

--- test-pages/login/login.php
+++ test-pages/login/login.php
@@ -1,7 +1,20 @@
 <?php
 	require '/var/www/test-pages/login/core/init.php';
 	if (Input::exists()) {
-		echo "Hello";
+		if(Token::check(Input::get('token'))){
+
+			$validate = new Validate();
+			$validation = $validate->check(array(
+				'username'  => array('required' => true),
+				'password'  => array('required' => true)
+			));
+
+			if($validation->passed()){
+				# log user in
+			}else{
+				$go = true;
+			}
+		}
 	}
 ?>
 
@@ -24,4 +37,18 @@
 			<input type="hidden" name="token" value="<?php echo Token::generate(); ?>" />
 			<tr><td colspan="2" style="text-align:center;"><input type="submit" value="Log In" /></td></tr>
 		</tbody>
+		<?php
+			if (isset($go)) {
+				?>
+					<h1>Errors:</h1>
+					<ol>
+				<?php
+					foreach ($validation->errors() as $error) {
+						echo "<li>{$error}</li>";
+					}
+				?>
+					</ol>
+				<?php
+			}
+		?>
 </form>
