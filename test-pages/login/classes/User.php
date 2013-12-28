f9da11f9c133f98a5d0b0e28c096b31aa098efd4
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-28 15:54:01 -0500

    added new method

--- test-pages/login/classes/User.php
+++ test-pages/login/classes/User.php
@@ -3,6 +3,7 @@ class User{
 	private $_db,
 			$_data,
 			$_sessionName,
+			$_cookieName,
 			$_isLoggedIn;
 
 	public function __construct($user = null)
@@ -10,6 +11,7 @@ class User{
 		$this->_db = DB::getInstance();
 
 		$this->_sessionName = Config::get("session/session_name");
+		$this->_cookieName = Config::get("remember/cookie");
 
 		if (!$user) {
 			if(Session::exists($this->_sessionName)){
@@ -54,12 +56,31 @@ class User{
 		}
 	}
 
-	public function login($username = null, $password = null)
+	public function login($username = null, $password = null, $remember)
 	{
 		$user = $this->find($username);
 		if ($user) {
 			if ($this->data()->password === Hash::make($password, $this->data()->salt)) {
 				Session::put($this->_sessionName, $this->data()->id);
+
+				if($remember){
+					$hash = Hash::uniqe();
+					$hashCheck = $this->_db->get(Config::get('mysql/table/session'), array('user_id', '=', $this->data()->id));
+
+					# insert data into the stuff
+					if(!$hashCheck->count()){
+						$this->_db->insert(array(
+							'user_id'		=> $this->data()->id,
+							'hash'			=> $hash
+						));
+					}else{
+						$hash = $hashCheck->first()->hash;
+					}
+
+					Cookie::put($this->_cookieName, $hash, Config::get('remember/cookie_expiry'));
+
+				}
+
 				return true;
 			}
 		}
