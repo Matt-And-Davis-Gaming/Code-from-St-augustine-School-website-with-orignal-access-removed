6d20430c6ffd0af0425d22dac55600497695dc61
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-28 15:55:30 -0500

    I am an idiot

--- test-pages/login/classes/User.php
+++ test-pages/login/classes/User.php
@@ -64,7 +64,7 @@ class User{
 				Session::put($this->_sessionName, $this->data()->id);
 
 				if($remember){
-					$hash = Hash::uniqe();
+					$hash = Hash::unique();
 					$hashCheck = $this->_db->get(Config::get('mysql/table/session'), array('user_id', '=', $this->data()->id));
 
 					# insert data into the stuff
