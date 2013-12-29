9a930b339ccc002ef2586ca94093e477300ee979
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-28 22:48:18 -0500

    changed stuff

--- test-pages/login/classes/Log.php
+++ test-pages/login/classes/Log.php
@@ -1,10 +1,12 @@
 <?php
 class Log{
-	public function log($action = 'defaulted', $name = 'NIL', $type = 'none'){
-		DB::getInstance()->insert(Config::get('mysql/table/logs/log'), array(
-			'type'			=> $type,
-			'text'			=> "{$name} has {$action} at " . date('Y-m-d H:i:s'),
-			'date'			=> date('Y-m-d H:i:s')
-		));
+	public function log($action = null, $name = null, $type = null){
+		if(isset($action, $name, $type)):
+			DB::getInstance()->insert(Config::get('mysql/table/logs/log'), array(
+				'type'			=> $type,
+				'text'			=> "{$name} has {$action} at " . date('Y-m-d H:i:s'),
+				'date'			=> date('Y-m-d H:i:s')
+			));
+		endif;
 	}
 }
