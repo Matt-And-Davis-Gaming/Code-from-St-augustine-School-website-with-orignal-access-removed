ae3bfa6d59b05cf1704e28e5f9370ffa4ddbd461
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-28 22:39:55 -0500

    created datetime peram

--- test-pages/login/classes/Log.php
+++ test-pages/login/classes/Log.php
@@ -3,8 +3,8 @@ class Log{
 	public function log($action = 'defaulted', $name = 'NIL'){
 		DB::getInstance()->insert(Config::get('mysql/table/logs/log'), array(
 			'type'			=> 'logout',
-			'text'			=> "{$name} has {$action} at " . date(''),
-			'date'			=> date()
+			'text'			=> "{$name} has {$action} at " . date('Y-m-d H:i:s'),
+			'date'			=> date('Y-m-d H:i:s')
 		));
 	}
 }
