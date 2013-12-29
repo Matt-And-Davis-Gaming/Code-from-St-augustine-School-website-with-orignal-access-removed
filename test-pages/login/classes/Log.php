e11b4a53b73903cd367fc8a4177a7efcdfb0faf0
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-28 22:02:07 -0500

    changed Log.php

--- test-pages/login/classes/Log.php
+++ test-pages/login/classes/Log.php
@@ -1,22 +1,10 @@
 <?php
-class Cookie{
-	public static function exists($cook)
-	{
-		return (isset($_COOKIE[$cook])) ? true : false ;
-	}
-	public static function get($name)
-	{
-		return $_COOKIE[$name];
-	}
-	public static function put($name, $value, $expiry)
-	{
-		if(setcookie($name, $value, time() + $expiry, '/')){
-			return true;
-		}else{
-			return false;
-		}
-	}
-	public static function delete($name){
-		self::put($name, '', time() - 1);
+class Log{
+	public static function log($action = 'defaulted', $name = 'NIL'){
+		DB::getInstance()->insert(Config::get('mysql/table/logs/log'), array(
+			'type'			=> 'logout',
+			'text'			=> "{$name} has {$action} at " . date(''),
+			'date'			=> date()
+		));
 	}
 }
