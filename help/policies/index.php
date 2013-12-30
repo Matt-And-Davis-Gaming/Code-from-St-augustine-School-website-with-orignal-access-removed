29de8f290c7db2c84a997ce797e3766591adebea
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-29 22:18:06 -0500

    added the policies page

---
+++ help/policies/index.php
@@ -0,0 +1,33 @@
+<?php
+	require('init.php');
+	head("Policies");
+	?>
+	<div class="jumbotron" style="padding-top:100px;">
+      <div class="container">
+        <h1>Polocies</h1>
+        <p>This page contains the human readable summary of the full <a href="#fulll">Legal Code</a></p>
+        <p><a class="btn btn-primary btn-lg" role="button" href="#">Learn more &raquo;</a></p>
+      </div>
+    </div>
+	<div class="container">
+		<style type="text/css">
+			div.row div.col-md-2{
+				position:fixed;
+				top:30px;
+				right:5px;
+			}
+		</style>
+		<!--[if lt IE 7]>
+			<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://chrome.google.com/">upgrade your browser</a> to get the bes$
+	        <![endif]-->
+	    <div class="row">
+	    	<div class="col-md-10">
+
+	    	</div>
+	    	<div class="col-md-2">
+	    		Thanks for reading the legal code! <a href="#">Return to top?</a>
+	    	</div>
+	    </div>
+	</div>
+<body>
+</html>
