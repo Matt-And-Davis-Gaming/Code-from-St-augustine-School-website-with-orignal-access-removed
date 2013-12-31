78efa3abb74308d00bc172faf3f5561bf72e406f
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-31 17:02:03 -0500

    started profile.php

---
+++ test-pages/login/profile.php
@@ -0,0 +1,16 @@
+<?php
+	require '/var/www/test-pages/login/core/init.php';
+	ini_set('display_errors', '1');
+	error_reporting(-1);
+
+	if(!$username = Input::get('user')){
+		Redirect::to('inden.php');
+	}else{
+		$user = new User($username);
+		if(!$user->exists()){
+			Redirect::(404);
+		}else{
+			echo "Exists";
+		}
+	}
+?>
