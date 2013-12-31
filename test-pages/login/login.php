8d71c016ae08f88b7788741b0c59954ef318886b
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-31 16:52:05 -0500

    Changed the login file to redirect if the session exists

--- test-pages/login/login.php
+++ test-pages/login/login.php
@@ -40,6 +40,8 @@
 			echo "<a href=\"http://en.wikipedia.org/wiki/Cross-site_request_forgery\">CSRF request detected. Click to read more.</a>
 			<br>If you still have any questions, visit our help guidelines at <a href=\"/help/policies#csrf\"> CSRF F.A.Q.</a>";
 		}
+	}elseif(Session::exists(Config::get('session/session_name'))){
+		Redirect::to('inden.php');
 	}else{
 		echo "Please enter the info below";
 	}
