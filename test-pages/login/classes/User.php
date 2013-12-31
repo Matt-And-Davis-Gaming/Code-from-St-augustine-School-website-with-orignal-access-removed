26769a85dacbf765ff088312d50c6643a82fbb77
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-30 21:00:00 -0500

    idiotness again

--- test-pages/login/classes/User.php
+++ test-pages/login/classes/User.php
@@ -138,7 +138,7 @@ class User{
 		$group = $this->_db->get(Config::get('mysql/table/groups'), array('id', '=', $this->data()->group));
 
 		if ($group->count()) {
-			$permissions = json_decode($group->first()->permissions);
+			$permissions = json_decode($group->first()->permissions, true);
 
 			if($permissions[$key] == true){
 				return true;
