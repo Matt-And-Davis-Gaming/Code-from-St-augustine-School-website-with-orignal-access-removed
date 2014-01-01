2daca9061b29ecaf6a0cef6525876bd85eabfee6
Matthew Krueger <mcolekrueger@gmail.com>
2014-01-01 10:16:44 -0500

    worked with the in.php

---
+++ func/page/in.php
@@ -0,0 +1,22 @@
+<?php
+	$u = new User();
+
+	if($u->isLoggedIn()){
+		echo "Hello " $u->data()->name;
+	}else{
+		?>
+
+		<form style="float: right;" class="navbar-form nav-center" method="post" action="/prossing.php">
+			<div class="form-group">
+				<input type="text" placeholder="Username" class="form-control" name = "username">
+				<input type="hidden" name="action" value="signIn" />
+				<input type="hidden" name="token" value="<?php echo Token::generate(); ?>" />
+				<input type="hidden" name="remember" id="remember" value="on"/>
+			</div>
+			<div class="form-group">
+				<input type="password" placeholder="Password" class="form-control" name="password">
+			</div>
+			<button type="submit" class="btn btn-success">Sign in</button>
+		</form>
+		<?php
+}
