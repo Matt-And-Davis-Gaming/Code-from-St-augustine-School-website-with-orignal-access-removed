4108c41cf452a776829e4f658adaa6183ac2b8cf
Matthew Krueger <mcolekrueger@gmail.com>
2013-12-31 21:37:48 -0500

    added new index and worked with account control

--- account-control/index.php
+++ account-control/index.php
@@ -13,23 +13,43 @@
 		<!--[if lt IE 7]>
 			<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://chrome.google.com/">upgrade your browser</a> to get the bes$
 	        <![endif]-->
-		<div class="row">
-        		<div class="col-md-4">
-          			<h2>Supporting Evidance</h2>
-          				<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          				<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        		</div>
-        		<div class="col-md-4">
-         			<h2>Supporting Evidance</h2>
-          				<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          				<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-       			</div>
-        		<div class="col-md-4">
-          			<h2>Supporting Evidance</h2>
-          				<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          				<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        		</div>
-		</div>
+        <?php
+          //echo PHP_VERSION;
+          ini_set('display_errors', '1');
+          error_reporting(-1);
+          require '/var/www/func/login/core/init.php';
+          # die(Config::get('mysql/table/session'));
+          /*$user = DB::getInstance()->update('users', 3, array(
+            'username'  => 'Dale',
+            'password'  => 'new_hashed_passphrase',
+            'salt'    => 'salt',
+            'name'    => 'Dale Garrett'
+          ));*/
+
+          # echo "<pre>", print_r($_SESSION), "</pre>";
+          # echo "User id is: ";
+          $user = new User();
+          if($user->isLoggedIn()){
+            ?>
+              <p>Hello <a href="profile.php?user=<?php echo escape($user->data()->username); ?>"><?php echo escape($user->data()->name); ?></a>!</p>
+
+              <ol>
+                <li><a href="logout.php">Log out</a></li>
+                <li><a href="update.php">Update user information</a></li>
+                <li><a href="changepassword.php">Change your password</a></li>
+              </ol>
+            <?php
+
+            if ($user->hasPermission('admin')) {
+              echo "<p>You are an administrator</p>";
+            }
+            
+            
+
+          }else{
+            Redirect::to('login');
+          }
+        ?>
 	</div>
 <body>
 </html>
