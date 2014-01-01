cc91ec4293811a235e917992ed15ca3ebc37c821
Matthew Krueger <mcolekrueger@gmail.com>
2014-01-01 09:18:10 -0500

    set error reporting option

--- account-control/flash.php
+++ account-control/flash.php
@@ -1,6 +1,9 @@
 <h1>
 <?php
+ini_set('display_errors', '1');
+			error_reporting(-1);
 require '/var/www/func/login/core/init.php';
+
 if(Session::exists('home')){
   # echo "<pre>", print_r($_SESSION), "</pre>";
   echo Session::flash("home");
