8d2c149d59bf1d3492815f8039605ef67a203214
Matthew Krueger <mcolekrueger@gmail.com>
2014-01-01 09:15:05 -0500

    added a flash.php

---
+++ account-control/flash.php
@@ -0,0 +1,14 @@
+<h1>
+<?php
+require '/var/www/func/login/core/init.php';
+if(Session::exists('home')){
+  # echo "<pre>", print_r($_SESSION), "</pre>";
+  echo Session::flash("home");
+  header("Refresh: 3; login.php");
+}
+if(Session::exists('in')){
+	echo Session::flash('in');
+	header('Refresh: 3; ./');
+}
+?>
+</h1>
