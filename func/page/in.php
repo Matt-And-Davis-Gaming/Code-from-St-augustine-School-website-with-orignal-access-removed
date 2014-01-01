01de8d0191adacbe1223b19fa948613b471fda1d
Matthew Krueger <mcolekrueger@gmail.com>
2014-01-01 10:25:49 -0500

    more css

--- func/page/in.php
+++ func/page/in.php
@@ -2,7 +2,7 @@
 	$u = new User();
 
 	if($u->isLoggedIn()){
-		echo "<div style=\"float: right;text-shadow: 0 -1px 0 rgba(0,0,0,0.25);color:#999;\">Hello " . $u->data()->name . "</div>";
+		echo "<div style=\"float: right;text-shadow: 0 -1px 0 rgba(0,0,0,0.25);color:#999;padding-top:10px;padding-bottom:10px;\">Hello " . $u->data()->name . "</div>";
 	}else{
 		?>
 
