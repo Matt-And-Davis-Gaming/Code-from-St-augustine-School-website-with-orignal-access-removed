796b33ab668c2ba51bf736fc3e3ed62ee0d2d0db
Matthew Krueger <mcolekrueger@gmail.com>
2014-01-06 16:54:48 -0500

    added some stuff

--- func/bleep/bleep.php
+++ func/bleep/bleep.php
@@ -1,11 +1,21 @@
 <?php
 
+function d($value,$replace)
+{
+	$swearo[] = $value;
+	$swearr[] = $replace;
+}
+
 function bleep($p){
 	$swearo = array();
 	require '/var/www/func/bleep/swearo.php';
 	
 	$swearr = array();
 	require '/var/www/func/bleep/swearr.php';
+
+	d('fuck', '****');
+
+	$p = str_ireplace($swearo, $swearr, $p);
 
 	return $p;
 }
