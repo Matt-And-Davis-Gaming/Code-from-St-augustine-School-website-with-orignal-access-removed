c71fb721297ef758c8231f0d4df43f8c698d4ad9
Matthew Krueger <mcolekrueger@gmail.com>
2014-01-06 10:28:33 -0500

    added new alert

--- test-pages/chat/inden.php
+++ test-pages/chat/inden.php
@@ -2,8 +2,30 @@
 <html>
 	<head>
 		<title>Chat</title>
+
+		<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js" async></script>
+		<script type="text/javascript">
+			var chat = {}
+			chat.fetchMessages = function(){
+
+			};
+
+			$('#input').keydown(function(event){
+				if ( event.which == 13 && event.shiftKey == false) {
+					event.preventDefault();
+					alert($('#input').val());
+				}
+			});
+		</script>
 	</head>
 	<body>
+		<div id="messages">
 
+		</div>
+		<textarea id="input">
+
+		</textarea>
 	</body>
 </html>
