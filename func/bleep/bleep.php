8e13e59068819f6af2e763f57f9ad97c25dc50af
Matthew Krueger <mcolekrueger@gmail.com>
2014-01-06 16:09:57 -0500

    some stuff

--- func/bleep/bleep.php
+++ func/bleep/bleep.php
@@ -2,9 +2,10 @@
 
 function bleep($p){
 	$swearo = array();
-
+	require '/var/www/func/bleep/swearo.php';
 	
 	$swearr = array();
+	require '/var/www/func/bleep/swearr.php';
 
 	return $p;
 }
---
+++ func/bleep/swearo.php
@@ -0,0 +1,1 @@
+swearo.php
