c937db6012d96f0c5f05ff7c55efb4529f2118b6
Matthew Krueger <mcolekrueger@gmail.com>
2014-01-06 10:59:07 -0500

    changed some css

--- test-pages/chat/inden.php
+++ test-pages/chat/inden.php
@@ -5,12 +5,15 @@
 
 		<style type="text/css">
 			#messages{
-				width:90%;
+				width: 90%;
+				left: 0;
+				right: 0;
 				text-align: left;
 				overflow-y: scroll;
 				overflow-x: hidden;
 				height:290px;
-				box-shadow: 
+				-webkit-box-shadow: rgba(0, 0, 128, 0.247059) 5px 7px 9px;
+				box-shadow: 5px 7px 9px;
 			}
 			#input{
 
