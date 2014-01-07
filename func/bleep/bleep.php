e6d4c782507419f660982220ed3b25ef38c49c33
Matthew Krueger <mcolekrueger@gmail.com>
2014-01-07 12:10:25 -0500

    added a disclaimer

--- func/bleep/bleep.php
+++ func/bleep/bleep.php
@@ -8,6 +8,10 @@ function bleep($p){
 	
 	$swearr = array();
 	require '/var/www/func/bleep/swearr.php';
+
+	# the words listed here are only for filtering purposes and are never showen to the visitors
+	# if you are conserned about the contents of this file, please go put a sock in it. This is to
+	# protect the children.
 	
 	$swearo[] = 'fuck';
 	$swearr[] = '****';
