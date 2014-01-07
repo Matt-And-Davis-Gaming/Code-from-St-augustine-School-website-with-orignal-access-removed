4526a6e8b59839fa6154f7ce97321a0f88783ae2
Matthew Krueger <mcolekrueger@gmail.com>
2014-01-07 12:03:15 -0500

    worked some

--- func/bleep/bleep.php
+++ func/bleep/bleep.php
@@ -1,19 +1,22 @@
 <?php
 
-function d($value,$replace)
-{
-	$swearo[] = $value;
-	$swearr[] = $replace;
-}
-
 function bleep($p){
 	$swearo = array();
 	require '/var/www/func/bleep/swearo.php';
+
+	
 	
 	$swearr = array();
 	require '/var/www/func/bleep/swearr.php';
-
-	d('fuck', '****');
+	
+	$swearo[] = 'fuck';
+	$swearr[] = '****';
+	$swearo[] = 'fück';
+	$swearr[] = '****';
+	$swearo[] = 'shit';
+	$swearr[] = '****';
+	$swearo[] = 'sh1t';
+	$swearr[] = '****';
 
 	$p = str_ireplace($swearo, $swearr, $p);
 
