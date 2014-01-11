b6aafc9388d5fe7700cc77865c5c4204fecbf32d
mattkruegerkarate <mattkruegerkarate@users.noreply.github.com>
2014-01-11 09:45:28 -0500

    ?

--- chat_post.php
+++ chat_post.php
@@ -5,30 +5,28 @@ error_reporting(-1);
 	# require '/var/www/func/bleep/bleep.php';
 	# echo Input::get('message');
 	if(Input::exists()){
-		# bleep message
-		#$blee = bleep(Input::get('message'));
+			# bleep message
+			#$blee = bleep(Input::get('message'));
+			
+			#die(print_r($blee));
+			# die('THere is input');
 		
-		#die(print_r($blee));
-		die('THere is input');
-	}else{
-		die('no input');
+		# the only reason why I swear here is to test it
+		$mess = trim($blee[0]);
+		
+		# create database instance
+		DB::getInstance();
+		
+		# insert if bleep passed
+		
+		$user = new User();
+		
+		if($blee[1] == true){
+			DB::getInstance()->insert(Config::get('mysql/table/chat1'), array(
+	                	'user_id' => $user->data()->id,
+	                	'message' => $blee[1]
+	                ));
+		}
 	}
-	# the only reason why I swear here is to test it
-	$mess = trim($blee[0]);
-	
-	# create database instance
-	DB::getInstance();
-	
-	# insert if bleep passed
-	
-	$user = new User();
-	
-	if($blee[1] == true){
-		DB::getInstance()->insert(Config::get('mysql/table/chat1'), array(
-                	'user_id' => $user->data()->id,
-                	'message' => $blee[1]
-                ));
-	}
-	
 
 ?>
