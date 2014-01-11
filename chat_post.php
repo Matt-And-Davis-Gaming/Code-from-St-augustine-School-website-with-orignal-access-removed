731d061350f349831c2d9a068212db4eafcba4a1
mattkruegerkarate <mattkruegerkarate@users.noreply.github.com>
2014-01-11 09:31:40 -0500

    worked some

--- chat_post.php
+++ chat_post.php
@@ -1,18 +1,29 @@
 <?php
-
+ini_set('display_errors', '1');
+error_reporting(-1);
 	require '/var/www/func/login/core/init.php';
 	require '/var/www/func/bleep/bleep.php';
 	# echo Input::get('message');
+	
+	# bleep message
+	$blee = bleep(Input::get('message'))
 
 	# the only reason why I swear here is to test it
-	$mess = trim(bleep(Input::get('message')));
+	$mess = trim($blee[0]);
 	
 	# create database instance
 	DB::getInstance();
 	
 	# insert if bleep passed
 	
+	$user = new User();
 	
+	if($blee[1] == true){
+		DB::getInstance()->insert(Config::get('mysql/table/logs/log'), array(
+                	'user_id' => $user->data()->id,
+                	'message' => $blee[1];
+                ));
+	}
 	
 
 ?>
