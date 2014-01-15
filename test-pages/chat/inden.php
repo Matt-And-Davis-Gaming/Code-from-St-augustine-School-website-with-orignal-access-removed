23dfdfb1b8c5fee5e9d10429c32855ef8605aa8e
Matthew Krueger <mcolekrueger@gmail.com>
2014-01-14 19:39:27 -0500

    ?

--- test-pages/chat/inden.php
+++ test-pages/chat/inden.php
@@ -14,7 +14,7 @@
 				height:290px;
 				-webkit-box-shadow: rgba(0, 0, 128, 0.247059) 5px 7px 9px;
 				box-shadow: 5px 7px 9px;
-				border: 2px dashed #ffffff;
+				border: 2px dashed black;
 				padding-left: 5px;
 			}
 			#input{
