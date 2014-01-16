f3f194b4cbb67997bb2bb1409c2f5140c7d5c1cd
mattkruegerkarate <mattkruegerkarate@users.noreply.github.com>
2014-01-16 10:34:25 -0500

    that should work

--- account-control/flash.php
+++ account-control/flash.php
@@ -7,11 +7,15 @@ require '/var/www/func/login/core/init.php';
 if(Session::exists('home')){
   # echo "<pre>", print_r($_SESSION), "</pre>";
   echo Session::flash("home");
-  header("Refresh: 10; login.php");
+  #header("Refresh: 10; login.php");
+  	sleep(2);
+	Redirect::to('login.php');
 }
 if(Session::exists('in')){
 	echo Session::flash('in');
-	header('Refresh: 10; ./');
+	#header('Refresh: 10; ./');
+	sleep(2);
+	Redirect::to('./');
 }
 ?>
 </h1>
