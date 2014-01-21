f4dfc7b43ce7e1c194bd0968544812b27bfa7228
Matthew Krueger <mcolekrueger@gmail.com>
2014-01-21 16:44:22 -0500

    didi some stuff

--- users/process.php
+++ users/process.php
@@ -1,4 +1,17 @@
 <?php
+
+if($_GET['path']){
+	$config = $_GET['path'];
+	$path = explode('/', $_GET['path']);
+
+	/*foreach($path as $bit){
+		if(isset($config[$bit])){
+			$config = $config[$bit];
+		}
+	}*/
+
+	# return $config;
+}
 echo "<pre>";
-print_r($_GET);
+	print_r($path);
 echo "</pre>";
