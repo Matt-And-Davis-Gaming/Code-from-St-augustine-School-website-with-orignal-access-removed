b650643f3a66c74b4736e8a2d617631ca19d2ad3
Matthew Krueger <mcolekrueger@gmail.com>
2014-02-15 18:40:13 -0500

    made new index for the "more" page

---
+++ more/index.php
@@ -0,0 +1,35 @@
+<?php
+	require('../init.php');
+	head("More");
+?>
+	     <div class="jumbotron" style="padding-top:100px;">
+      <div class="container">
+        <h1>More from St. Augustine!</h1>
+        <p></p>
+        <p><-- <a class="btn btn-primary btn-lg" role="button">Learn more &raquo;</a> --></p>
+      </div>
+    </div>
+	<div class="container">
+		<!--[if lt IE 7]>
+			<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://chrome.google.com/">upgrade your browser</a> to get the bes$
+	        <![endif]-->
+		<div class="row">
+        		<div class="col-md-4">
+          			<h2>Supporting Evidence</h2>
+          				<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          				<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        		</div>
+        		<div class="col-md-4">
+         			<h2>Supporting Evidence</h2>
+          				<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          				<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+       			</div>
+        		<div class="col-md-4">
+          			<h2>Supporting Evidence</h2>
+          				<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          				<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        		</div>
+		</div>
+	</div>
+<body>
+</html>
