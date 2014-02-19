81aeac40f09c08a80095764ceba7390a1df02b03
Matthew Krueger <mcolekrueger@gmail.com>
2014-02-18 21:37:53 -0500

    new swift

---
+++ mail.php
@@ -0,0 +1,32 @@
+<?php
+require("/pass.php");
+function SendCookieToTheMail()
+{
+    require_once 'swift-mailer/lib/swift_required.php';
+    //Create the Transport
+    $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com')
+      ->setPort(465)
+      ->setEncryption('ssl')
+      ->setUsername('mcolekrueger@gmail.com')
+      ->setPassword(GMAIL_PASS)
+      ;
+
+    //Create the Mailer using your created Transport
+    $mailer = Swift_Mailer::newInstance($transport);
+
+    //Create a message
+    $message = Swift_Message::newInstance('Test')
+      ->setFrom(array('no_reply@staugustineschool.org' => 'From mr. 007'))
+      ->setTo(array('mcolekrueger@gmail.com'))
+      ->setBody('Body')
+      ;
+
+    //Send the message
+    $result = $mailer->send($message);
+
+    /*
+    You can alternatively use batchSend() to send the message
+
+    $result = $mailer->batchSend($message);
+    */ 
+}
