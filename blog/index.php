51fa84e79ce05cd5ea0cadba1ab77e690e432b7a
Matthew Krueger <mcolekrueger@gmail.com>
2014-03-12 12:05:29 -0400

    made new user class. I have no clue why it is not in the init file

--- blog/index.php
+++ blog/index.php
@@ -4,6 +4,7 @@ require 'resources/init.php';
   	     <div class="jumbotron" style="padding-top:100px;">
       <div class="container">
       	<?php
+      	$user = new User();
       	if($user->isLoggedIn()){
         	echo "<h1>Hello <a href=\"/user/" . escape($user->data()->username) . "\">" . escape($user->data()->name) . "</a>!</h1>";
     	}else{
