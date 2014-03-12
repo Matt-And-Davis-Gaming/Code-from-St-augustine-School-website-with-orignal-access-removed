0a8fdd05fe551c5a0265c10a5c96246774ab82b9
Matthew Krueger <mcolekrueger@gmail.com>
2014-03-12 11:57:38 -0400

    ok, did an outline

---
+++ blog/resources/init.php
@@ -0,0 +1,25 @@
+<?php
+  require('../init.php');
+  head("Account Control");
+  ?>
+  	     <div class="jumbotron" style="padding-top:100px;">
+      <div class="container">
+        <h1>Hello <a href="/user/<?php echo escape($user->data()->username); ?>"><?php echo escape($user->data()->name); ?></a>!</h1>
+        <p>Welcome to the blog of St. Augustine!</p>
+      </div>
+    </div>
+	<div class="container">
+		<!--[if lt IE 7]>
+			<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://chrome.google.com/">upgrade your browser</a> to get the bes$
+	        <![endif]-->
+            <?php
+
+            if ($user->hasPermission('admin')) {
+              echo "<p>You are an administrator</p>";
+            }
+          ?>
+
+
+	</div>
+<body>
+</html>
